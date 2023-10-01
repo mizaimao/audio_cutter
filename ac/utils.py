2ccd25d129bf4ec2d3c27e4309b673b26477913f
mizaimao <frankzhang930@gmail.com>
2023-09-30 23:21:02 -0400

    utils script

---
+++ ac/utils.py
@@ -0,0 +1,30 @@
+"""Helper function and small modules."""
+
+from typing import List
+
+def timestamp_check(
+    start_min: int,
+    start_sec: int,
+    start_msec: int,
+    end_min: int,
+    end_sec: int,
+    end_msec: int,
+) -> str:
+    time_error_flag: bool = True
+    if start_min < end_min:
+        time_error_flag = False
+    elif start_min == end_min:
+        if start_sec < end_sec:
+            time_error_flag = False
+        elif start_sec == end_msec:
+            if start_msec < end_msec:
+                time_error_flag = False
+            else:
+                pass
+        else:
+            pass
+    else:
+        pass
+
+    if time_error_flag:
+        return "Start timestamp is larger than or equal to end timestamp."
